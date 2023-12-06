ed05152f6ea096691dfd3bf40263fcb52c7116f5
Rodrigo Sousa <rodrigo.sdeveloper@gmail.com>
2023-12-06 11:36:33 -0300

    Configuração inicial
    Preparando a API. Configurações inicias.

---
+++ index.js
@@ -0,0 +1,45 @@
+// Importando o express
+import express from "express";
+
+// Importando a rota Accounts
+import acoountsRouter from "./routes/accounts.js";
+
+// Importando o file system, para podermos trabalhar com a manipulação de arquivos.
+import { promises as fs} from "fs";
+
+// Deestruturando as funções de leitura e escrita do file system. Podendo utilizar cada uma das variáveis sozinhas
+const { readFile, writeFile } = fs;
+
+// Instanciando o express
+const app = express();
+
+// Solicitando ao express que utilize JSON, para as requisições que iremos utilizar.
+app.use(express.json());
+
+// Associando o nosso router a instância do Express
+app.use("/account", acoountsRouter);
+
+// Iniciando a API.
+app.listen(3000, async ()=>{
+    // Verificando se o arquivo existe
+    try{
+        // Realizando a leitura, como utilizamos promises, iremos usar o async await
+        await readFile("accounts.json");
+        // Verificar se o arquivo .json que iremos armazenar os dados existe, senão criando o mesmo.
+        console.log("API Started!");
+    }catch (err){
+        // Variável para iniciar o arquivo JSON, como uma estrutura básica
+        const initialJson = {
+            nextId: 1,
+            accounts: []
+        }
+        // Criando o arquivo. Utilizando JSON.stringify, para converter a estrutura JSON em arquivo .json
+        writeFile("accounts.json", JSON.stringify(initialJson)).then(()=>{
+            // Verificar se o arquivo .json que iremos armazenar os dados existe, senão criando o mesmo.
+            console.log("API Started and File Created!");
+        }).catch(err =>{
+            console.log(err);
+        });
+    }
+    
+});
---
+++ routes/accounts.js
@@ -0,0 +1,11 @@
+// Importando o express
+
+import express from "express";
+
+// Criando o objeto para o roteador
+
+const router = express.Router();
+
+// Exportando esta rota, para utilizarmos em outros arquivos
+
+export default router;
